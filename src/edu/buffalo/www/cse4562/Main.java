951307beebf2d170df34000a14d1be0f12d74c24
Prashant Mishra <prashant.mishra.in@gmail.com>
2018-02-03 20:24:12 -0500

    Fixed typo.

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -3,7 +3,7 @@ package edu.buffalo.www.cse4562;
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
     }
 
 }
