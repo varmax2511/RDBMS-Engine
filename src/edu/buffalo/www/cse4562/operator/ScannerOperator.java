1f4e9df9a4bd549bdbe46fced648908e04a6875a
Prashant Mishra <prashant.mishra.in@gmail.com>
2018-03-17 17:39:22 -0400

    562: Auto stash before merge of "prashant_cse562" and "master"
    Change chuck size.

--- src/edu/buffalo/www/cse4562/operator/ScannerOperator.java
+++ src/edu/buffalo/www/cse4562/operator/ScannerOperator.java
@@ -215,7 +215,7 @@ public class ScannerOperator extends Node implements UnaryOperator {
    */
   public static class Config {
     private final String dataParentPath;
-    private int chunkSize = 15;
+    private int chunkSize = 60;
     private final String tableName;
     private final String alias;
 
