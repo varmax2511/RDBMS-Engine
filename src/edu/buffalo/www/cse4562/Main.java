e36015229f38188ce59a244a601909e8c0c6e923
varmax2511 <var_max@hotmail.com>
2018-04-11 16:12:30 -0400

    bugfix

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -24,7 +24,7 @@ public class Main {
     System.out.println(ApplicationConstants.BASH);
     System.out.flush();
     String dataPath = args[1];
-    ApplicationConstants.DATA_DIR_PATH = dataPath;
+    ApplicationConstants.DATA_DIR_PATH = dataPath +  "/";
     
     final CCJSqlParser parser = new CCJSqlParser(System.in);
     Statement statement = parser.Statement();
