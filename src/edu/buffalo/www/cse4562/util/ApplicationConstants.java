46f3bdb9dc381cdc7724a54c0078b5f90b57d5d4
varmax2511 <var_max@hotmail.com>
2018-04-11 15:58:43 -0400

    - Configurable data path

--- src/edu/buffalo/www/cse4562/util/ApplicationConstants.java
+++ src/edu/buffalo/www/cse4562/util/ApplicationConstants.java
@@ -1,9 +1,9 @@
 package edu.buffalo.www.cse4562.util;
 
-public interface ApplicationConstants {
+public class ApplicationConstants {
 
-  public String BASH = "$> ";
-  public String DATA_DIR_PATH = "data/";
-  public String SUPPORTED_FILE_EXTENSION = ".dat";
+  public static String BASH = "$> ";
+  public static String DATA_DIR_PATH = "data/";
+  public static String SUPPORTED_FILE_EXTENSION = ".dat";
 
 }
